c758a03581f2aa92f46a83cfa695f6bdd19e54b1
Romi Julianto <romyjulians@gmail.com>
2022-10-19 17:48:08 +0700

    create auth model

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './core/database/database.module';
 import { UsersModule } from './modules/apps/users/users.module';
+import { AuthModule } from './modules/apps/auth/auth.module';
 
 @Module({
     imports: [
         ConfigModule.forRoot({ isGlobal: true }),
         DatabaseModule,
         UsersModule,
+        AuthModule,
     ]
 })
 export class AppModule { }
