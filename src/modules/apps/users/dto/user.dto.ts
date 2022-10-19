f2acfc27bb3e3bf314219ee9c34af395141e0086
Romi Julianto <romyjulians@gmail.com>
2022-10-19 19:13:12 +0700

    update user.dto for validation

--- src/modules/apps/users/dto/user.dto.ts
+++ src/modules/apps/users/dto/user.dto.ts
@@ -1,10 +1,37 @@
+import { IsNotEmpty, MinLength, IsEmail, IsEnum, IsDate } from 'class-validator';
+
+enum Gender {
+    LAKI_LAKI = 'Laki-laki',
+    PEREMPUAN = 'Perempuan',
+}
 export class UserDto {
+    @IsNotEmpty()
     readonly nik: string;
+
+    @IsNotEmpty()
     readonly name: string;
+
+    @IsNotEmpty()
     readonly phone: string;
+
+    @IsNotEmpty()
+    @IsEmail()
     readonly email: string;
+
+    @IsNotEmpty()
+    @MinLength(6)
     readonly password: string;
+
+    @IsNotEmpty()
+    @IsDate()
     readonly birth_date: Date;
+
+    @IsNotEmpty()
+    @IsEnum(Gender, {
+        message: 'Jenis Kelamin adalah Laki-laki/Perempuan',
+    })
     readonly gender: string;
+
+    @IsNotEmpty()
     readonly address: string;
 }
